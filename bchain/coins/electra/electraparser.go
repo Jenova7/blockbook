83f99ee94fb007a1d4981f7525219f084b5b8c4d
Jenova7 <38404798+Jenova7@users.noreply.github.com>
2019-08-21 17:26:38 +0200

    [WIP] parser

---
+++ bchain/coins/electra/electraparser.go
@@ -0,0 +1,64 @@
+package electra
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"blockbook/bchain/coins/utils"
+	"bytes"
+
+	"github.com/jakm/btcutil/chaincfg"
+	"github.com/btcsuite/btcd/wire"
+)
+
+const (
+	MainnetMagic wire.BitcoinNet = 0Xb5a2f1b4
+	TestnetMagic wire.BitcoinNet = 0xe1e2e3e7
+)
+
+var (
+	MainNetParams chaincfg.Params
+	TestNetParams chaincfg.Params
+)
+
+func init() {
+	MainNetParams = chaincfg.MainNetParams
+	MainNetParams.Net = MainnetMagic
+	MainNetParams.PubKeyHashAddrID = []byte{33} // 0x21 
+	MainNetParams.ScriptHashAddrID = []byte{40} // 0x28 
+	MainNetParams.PrivateKeyID = []byte{161} // 0xA1
+
+	TestNetParams = chaincfg.TestNet3Params
+	TestNetParams.Net = TestnetMagic
+	TestNetParams.PubKeyHashAddrID = []byte{92} // 0x5C	
+	TestNetParams.ScriptHashAddrID = []byte{41} // 0x29
+	MainNetParams.PrivateKeyID = []byte{220} // 0xDC
+}
+
+// ElectraParser handle
+type ElectraParser struct {
+	*btc.BitcoinParser
+}
+
+// NewElectraParser returns new ElectraParser instance
+func NewElectraParser(params *chaincfg.Params, c *btc.Configuration) *ElectraParser {
+	return &ElectraParser{BitcoinParser: btc.NewBitcoinParser(params, c)}
+}
+
+// GetChainParams contains network parameters
+func GetChainParams(chain string) *chaincfg.Params {
+	if !chaincfg.IsRegistered(&MainNetParams) {
+		err := chaincfg.Register(&MainNetParams)
+		if err == nil {
+			err = chaincfg.Register(&TestNetParams)
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+	switch chain {
+	case "test":
+		return &TestNetParams
+	default:
+		return &MainNetParams
+	}
+}
---
+++ bchain/coins/electra/electraparser_test.go
@@ -0,0 +1,57 @@
+// +build unittest
+
+package electra
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/hex"
+	"math/big"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/martinboehm/btcutil/chaincfg"
+)
+
+func TestMain(m *testing.M) {
+	c := m.Run()
+	chaincfg.ResetParams()
+	os.Exit(c)
+}
+
+func Test_GetAddrDescFromAddress_Mainnet(t *testing.T) {
+	type args struct {
+		address string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{/*
+			name:    "",
+			args:    args{address: ""},
+			want:    "",
+			wantErr: false,
+		},*/
+	
+	}
+
+	parser := NewElectraParser(GetChainParams("main"), &btc.Configuration{})
+
+		for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parser.GetAddrDescFromAddress(tt.args.address)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAddrDescFromAddress() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			h := hex.EncodeToString(got)
+			if !reflect.DeepEqual(h, tt.want) {
+				t.Errorf("GetAddrDescFromAddress() = %v, want %v", h, tt.want)
+			}
+		})
+	}
+}
